5f764d403620f8435d67e9439b227254d2086ad6
Eduardo <jeseduardopi@gmail.com>
2021-09-20 15:43:00 +0200

    add PORT env var for express.listen

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.use(
 );
 app.use(cookieParser());
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+const PORT = process.env.PORT || 5000;
+// here we change 5000 to PORT
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
 // set up routers
